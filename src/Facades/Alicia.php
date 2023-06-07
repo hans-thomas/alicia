704e5b42f32f636e9b8ad326eddd6506a5612685
mortazavi <s.mohammad027@live.com>
2023-06-07 16:36:03 +0330

    DocBlock created for facades;

--- src/Facades/Alicia.php
+++ src/Facades/Alicia.php
@@ -2,8 +2,26 @@
 
 	namespace Hans\Alicia\Facades;
 
+	use Hans\Alicia\Models\Resource;
+	use Hans\Alicia\Services\AliciaService;
+	use Illuminate\Http\UploadedFile;
+	use Illuminate\Support\Collection;
 	use Illuminate\Support\Facades\Facade;
 
+	/**
+	 * @method static AliciaService batch( array $files )
+	 * @method static AliciaService upload( UploadedFile $file )
+	 * @method static AliciaService external( string $file )
+	 * @method static AliciaService export( array $resolutions = null )
+	 * @method static bool delete( Resource|int $model )
+	 * @method static array batchDelete( array $ids )
+	 * @method static bool deleteFile( string $path )
+	 * @method static AliciaService makeExternal( Resource $model, string $url )
+	 * @method static AliciaService fromFile( string $path )
+	 * @method static Resource|Collection|null getData()
+	 *
+	 * @see AliciaService
+	 */
 	class Alicia extends Facade {
 
 		/**
--- src/Facades/Signature.php
+++ src/Facades/Signature.php
@@ -2,8 +2,17 @@
 
 	namespace Hans\Alicia\Facades;
 
+	use Hans\Alicia\Services\SignatureService;
 	use Illuminate\Support\Facades\Facade;
 
+	/**
+	 * @method static string create()
+	 * @method static string key()
+	 * @method static bool isNotValid( string $signature )
+	 * @method static bool isValid( string $signature )
+	 *
+	 * @see SignatureService
+	 */
 	class Signature extends Facade {
 
 		/**
