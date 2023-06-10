21be6c21454b589e28fe9ecc1fdc1ac881af44d9
mortazavi <s.mohammad027@live.com>
2023-06-10 11:16:56 +0330

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@
 	use Hans\Alicia\Http\Controllers\ResourceController;
 	use Illuminate\Support\Facades\Route;
 
-	Route::get( config( 'alicia.link' ), [ ResourceController::class, 'download' ] )->name( 'alicia.download' );
+	Route::get( alicia_config( 'link' ), [ ResourceController::class, 'download' ] )->name( 'alicia.download' );
