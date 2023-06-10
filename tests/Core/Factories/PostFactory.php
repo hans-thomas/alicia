d8234b1792fb018779d4c86426291014c6101fb7
mortazavi <s.mohammad027@live.com>
2023-06-10 14:58:28 +0330

    Core entity created;

---
+++ tests/Core/Factories/PostFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+	namespace Hans\Alicia\Tests\Core\Factories;
+
+	use Hans\Alicia\Tests\Core\Models\Post;
+	use Illuminate\Database\Eloquent\Factories\Factory;
+
+	class PostFactory extends Factory {
+		protected $model = Post::class;
+
+		/**
+		 * @return array
+		 */
+		public function definition() {
+			return [
+				'title'   => $this->faker->sentence(),
+				'content' => $this->faker->paragraph(),
+			];
+		}
+	}
---
+++ tests/Core/Models/Post.php
@@ -0,0 +1,28 @@
+<?php
+
+	namespace Hans\Alicia\Tests\Core\Models;
+
+	use Hans\Alicia\Tests\Core\Factories\PostFactory;
+	use Hans\Alicia\Traits\AliciaHandler;
+	use Illuminate\Database\Eloquent\Factories\Factory;
+	use Illuminate\Database\Eloquent\Factories\HasFactory;
+	use Illuminate\Database\Eloquent\Model;
+
+	class Post extends Model {
+		use HasFactory, AliciaHandler;
+
+		protected $fillable = [
+			'title',
+			'content',
+		];
+
+		/**
+		 * Create a new factory instance for the model.
+		 *
+		 * @return Factory<static>
+		 */
+		protected static function newFactory() {
+			return PostFactory::new();
+		}
+
+	}
