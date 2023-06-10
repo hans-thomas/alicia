c70e59fad3607130e2efd2c1c4631be8da23a555
mortazavi <s.mohammad027@live.com>
2023-06-10 14:53:18 +0330

    AliciaServiceTest created;

---
+++ tests/Feature/AliciaServiceTest.php
@@ -0,0 +1,21 @@
+<?php
+
+	namespace Hans\Alicia\Tests\Feature;
+
+	use Hans\Alicia\Facades\Alicia;
+	use Hans\Alicia\Tests\TestCase;
+
+	class AliciaServiceTest extends TestCase {
+
+		/**
+		 * @test
+		 *
+		 * @return void
+		 */
+		public function getData(): void {
+			$data = Alicia::getData();
+
+			self::assertNull( $data );
+		}
+
+	}
