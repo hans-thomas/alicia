291624b85d74f83bfdcc15be3af1dda367cb1df8
mortazavi <s.mohammad027@live.com>
2023-06-11 08:46:33 +0330

    write comments for functions;

--- src/Helpers/functions.php
+++ src/Helpers/functions.php
@@ -5,18 +5,39 @@
 	use Illuminate\Support\Str;
 
 	if ( ! function_exists( 'alicia_config' ) ) {
-		function alicia_config( string $key, mixed $default = null ) {
+		/**
+		 * Return alicia config
+		 *
+		 * @param string     $key
+		 * @param mixed|null $default
+		 *
+		 * @return mixed
+		 */
+		function alicia_config( string $key, mixed $default = null ): mixed {
 			return config( "alicia.{$key}", $default );
 		}
 	}
 
 	if ( ! function_exists( 'alicia_storage' ) ) {
+		/**
+		 * Return alicia storage
+		 *
+		 * @return Filesystem
+		 */
 		function alicia_storage(): Filesystem {
 			return Storage::disk( 'resources' );
 		}
 	}
 
 	if ( ! function_exists( 'generate_file_name' ) ) {
+		/**
+		 * Generate random file name
+		 *
+		 * @param string|null $driver
+		 * @param int         $length
+		 *
+		 * @return string
+		 */
 		function generate_file_name( string $driver = null, int $length = 16 ): string {
 			return match ( $driver ? : alicia_config( 'naming' ) ) {
 				'string' => Str::random( $length ),
@@ -34,6 +55,11 @@
 	}
 
 	if ( ! function_exists( 'get_classified_folder' ) ) {
+		/**
+		 * Return classified folder name
+		 *
+		 * @return string
+		 */
 		function get_classified_folder(): string {
 			if ( ! alicia_storage()->exists( $folder = alicia_config( 'classification' ) ) ) {
 				alicia_storage()->makeDirectory( $folder );
