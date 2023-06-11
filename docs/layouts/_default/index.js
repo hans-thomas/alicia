53fc6cd2aadd4f6b27a7a93faa32219a39d1e732
mortazavi <s.mohammad027@live.com>
2023-06-11 14:33:10 +0330

    docs added;

---
+++ docs/layouts/_default/index.js
@@ -0,0 +1,29 @@
+var docs = [
+  {{range $index, $page: = (where.Site.Pages "Section" "docs"
+)
+-
+}
+}
+{
+  id: {
+    {
+      $index
+    }
+  }
+,
+  title: "{{ .Title }}",
+    description
+:
+  "{{ .Params.description }}",
+    href
+:
+  "{{ .URL | relURL }}"
+}
+,
+{
+  {
+    end -
+  }
+}
+]
+;
